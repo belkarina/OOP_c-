cd8d992d0696290585fbd20a07c35385e9a6cbd4
belkarina <katya.filosofova@mail.ru>
2021-01-05 00:52:44 +0700

    The end of 3-rd lab, create class and work with objects.

--- OOP_c++/Source.cpp
+++ OOP_c++/Source.cpp
@@ -1,180 +1,171 @@
 #include "stdio.h"
+#include <iostream>
 #include "conio.h"
 #include <windows.h>
+#include <malloc.h>
 
-// Create struct
-struct grocerystore {
-	char country[20];
-	char prod[20];
+using namespace std;
+
+// Create class
+class Item {
+private:
+	string country;
+	string name;
 	double price;
+public:
+	// Init
+	Item() {
+		country = "";
+		name = "";
+		price = 0;
+	}
+	// Correct(?) init
+	Item(string newCountry, string newName, double newPrice) {
+		country = newCountry;
+		name = newName;
+		price = newPrice;
+	}
+	string getCountry() {
+		return(country);
+	}
+	string getName() {
+		return(name);
+	}
+	double getPrice() {
+		return(price);
+	}
+	void setCountry(string newCountry){
+		country = newCountry;
+	}
+	void setName(string newName) {
+		name = newName;
+	}
+	void setPrice(double newPrice) {
+		price = newPrice;
+	}
+
+
+	// Read 
+	static Item input(int i) {
+		string newCountry;
+		string newName;
+		double newPrice;
+		do {
+			cout << "Input country of " << i + 1 << " product: ";
+			rewind(stdin);
+			cin >> newCountry;
+		} while (newCountry[0] == '\0' || newCountry[0] == ' ');
+
+		do {
+			cout << "Input name of " << i + 1 << " product: ";
+			rewind(stdin);
+			cin >> newName;
+		} while (newName[0] == '\0' || newName[0] == ' ');
+
+		do {
+			cout << "Input price of " << i + 1 << " product: ";
+			rewind(stdin);
+			cin >> newPrice;
+			if (cin.fail())
+				cin.clear();
+		} while (newPrice <= 0);
+		cout << endl;
+		Item newItem = Item(newCountry, newName, newPrice);
+		return(newItem);
+	}
+	// Display
+	void output(int i) {
+		cout << i + 1 << " product:" << endl;
+		cout << "Country: " << country << endl;
+		cout << "Product: " << name << endl;
+		cout << "Price = " <<  price << endl << endl;
+	}
+
+	// Add 
+	static int add(Item* item, Item* secondItem) {
+		int sum;
+		sum = item->getPrice() + secondItem->getPrice();
+		return sum;
+	}
+
+	// Add sale
+	static void sale(Item* item, int num) {
+		for (int i = 0; i < num; i++)
+			(*item).setPrice((*item).getPrice() * 0.5);
+	}
+
+	// Add markup
+	void markup(Item* item, int num) {
+		for (int i = 0; i < num; i++)
+			(*item).setPrice((*item).getPrice() * 2);
+	}
 };
 
-// Read func
-grocerystore input(int i) {
-	grocerystore inputshop;
-	do {
-		rewind(stdin);
-		printf("Input country of %d product: ", i + 1);
-		gets_s(inputshop.country, 20);
-	} while (inputshop.country[0] == '\0' || inputshop.country[0] == ' ');
-
-	do {
-		rewind(stdin);
-		printf("Input name of %d product: ", i + 1);
-		gets_s(inputshop.prod, 20);
-	} while (inputshop.prod[0] == '\0' || inputshop.prod[0] == ' ');
-
-	int dop;
-	do {
-		rewind(stdin);
-		printf("Input price of %d product(for ex. 2081.98): ", i + 1);
-		dop = scanf_s("%lf", &inputshop.price);
-	} while (dop != 1 || inputshop.price < 0);
-
-	printf("\n");
-	return inputshop;
-}
-
-// Display func
-void output(int i, grocerystore outputshop) {
-	printf("%d product:\n", i + 1);
-	printf("Country: ");
-	puts(outputshop.country);
-	printf("Product: ");
-	puts(outputshop.prod);
-	printf("Price = %.2lf\n\n", outputshop.price);
-}
-
-// Init func
-grocerystore init() {
-	grocerystore initshop;
-	initshop.country[0] = ' ';
-	initshop.prod[0] = ' ';
-	initshop.price = 0;
-	return initshop;
-}
-
-// Add func
-void add(grocerystore* addshop, int num) {
-	printf("Do you want buy smth?(\'1\' - yes, else - no)\n");
-	if (_getch() == '1') {
-		printf("OK. How many products do you want? ");
-		int n;
-		rewind(stdin);
-		scanf_s("%d", &n);
-		rewind(stdin);
-		printf("What product numbers do you want?(if you want more than 1 same things - enter \"1 1 1 1...\") ");
-		int prod[20];
-		double sum = 0;
-		for (int i = 0; i < n; i++) {
-			scanf_s("%d", &prod[i]);
-			int dop = prod[i] - 1;
-			sum += addshop[dop].price;
-			if (prod[i] > num || prod[i] < 0) {
-				sum -= addshop[dop].price;
-				i--;
-				printf("Incorrect number of product, please, enter again.\n");
-			}
-		}
-		printf("\nSum price = %.2lf", sum);
-	}
-
-}
-
-// Add sale func
-void salestruct(struct grocerystore* shop, int num) {
-	for (int i = 0; i < num; i++)
-		shop[i].price = shop[i].price * 0.5;
-}
-
-// Add markup func
-void markupstruct(struct grocerystore* shop, int num) {
-	for (int i = 0; i < num; i++)
-		shop[i].price = shop[i].price * 2;
-}
 
 int main() {
-	int num, dop;
-	printf("How many products do you want to input?(positive number)\n");
+	
+	cout << "Now it is a static object:" << endl;
+	Item staticItem = Item();
+
+	cout << "Item created, Init used." << endl;
+	staticItem.output(0);
+
+	cout << "Now we need enter values of item:" << endl;
+	staticItem = Item::input(0);
+	staticItem.output(0);
+
+	cout << "If we are add 2 equal items, we will get:" << endl;
+	cout << "Sum price = " << Item::add(&staticItem, &staticItem) << endl << endl;
+	staticItem.output(0);
+
+	cout << "Sale! 50%!" << endl;
+	staticItem.sale(&staticItem, 1);
+	staticItem.output(0);
+
+	cout << "Sale is over!" << endl;
+	staticItem.markup(&staticItem, 1);
+	staticItem.output(0);
+	
+	cout << "Press any key to continue";
+	_getch();
+
+	system("cls");
+
+	cout << "Now it is a dynamic object:" << endl;
+	Item* dynamicItem = new Item("Something", "Somewhere", 100);
+	dynamicItem->output(0);
+	delete dynamicItem;
+
+	cout << "Press any key to continue" << endl;
+	_getch();
+
+	cout << "Now it is a dynamic array of objects:" << endl;
+	int num;
 	do {
+		cout << "How many objects do you want to input?" << endl;
 		rewind(stdin);
-		dop = scanf_s("%d", &num);
-		if (num < 1) printf("Please, input positive number\n");
-	} while (num < 1 || dop != 1);
+		cin >> num;
+		if (cin.fail())
+			cin.clear();
+	} while (num <= 0);
+	cout << endl;
 
-	if (num == 1) {																// non-dinamic struct
-		struct grocerystore shop[2];											// create struct in project
-		shop[0] = init();
-		shop[0] = input(0);
+	Item* badItem = (Item*)calloc(num, sizeof(Item));
+	for (int i = 0; i < num; i++)
+		badItem[i] = Item::input(i);
+ 
+	cout << "Now we are showing your last object:" << endl;
+	badItem[num - 1].output(num - 1);
 
-		output(0, shop[0]);
+	free(badItem);
 
-		printf("For add func we need one more struct. Please, input its. \nIf you are tired and don't want - you can press \'1\' right now and go out of the program. Otherwise - press any key.\n\n");
-		rewind(stdin);
-		if (_getch() == '1') {
-			printf("Thank you for honesty! Bye!");
-			_getch();
-			return 0;
-		}
-		shop[1] = init();
-		shop[1] = input(1);
+	Item* dynamicItem1 = new Item[5];
+	delete[] dynamicItem1;
 
-		output(0, shop[0]);
-		output(1, shop[1]);
-
-		add(shop, 2);
-
-		printf("\n\nSale! 50%!\n\n");
-		salestruct(shop, 2);
-
-		output(0, shop[0]);
-		output(1, shop[1]);
-
-		add(shop, 2);
-
-		printf("\nSale is over.\n");
-		markupstruct(shop, num);
-
-		output(0, shop[0]);
-		output(1, shop[1]);
-
-		printf("Program is over.");
-		_getch();
-	}
-	else {
-		struct grocerystore* shop;												// create dinamic struct in project
-		shop = (struct grocerystore*)malloc(num * sizeof(struct grocerystore));
-
-		for (int i = 0; i < num; i++) {
-			shop[i] = init();
-			shop[i] = input(i);
-		}
-
-		for (int i = 0; i < num; i++)
-			output(i, shop[i]);
-
-		add(shop, num);
-		_getch();
-
-		printf("\n\nSale! 50%!\n\n");
-		salestruct(shop, num);
-
-		for (int i = 0; i < num; i++)
-			output(i, shop[i]);
-
-		add(shop, num);
-
-		printf("Press any key to continue\n");
-		_getch();
-
-		printf("\nSale is over.\n");
-		markupstruct(shop, num);
-
-		for (int i = 0; i < num; i++)
-			output(i, shop[i]);
-
-		printf("Program is over.");
-		_getch();
-	}
+	Item* badItem2 = (Item*)malloc(10 * sizeof(Item));
+	badItem2 = (Item*)realloc(badItem2, 20 * sizeof(Item));
+	free(badItem2);
+	cout << "Program is over. Press any key to exit.";
+	_getch();
 	return 0;
 }
